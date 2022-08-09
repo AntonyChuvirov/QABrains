3d651a5f4ff17fe496c9568e207f600ba589fe5f
Ilona27 <ilona.kohan2003@gmail.com>
2022-08-09 11:40:42 +0300

    Language Button Test

---
+++ src/test/java/IlonaTests/Test/LanguageButtonTest.java
@@ -0,0 +1,25 @@
+package IlonaTests.Test;
+
+import BaseClasses.TestInit;
+import IlonaTests.Pages.CheckChangeLanguagePage;
+import IlonaTests.Pages.HomePage;
+import IlonaTests.Pages.LanguageBtnPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LanguageButtonTest  extends TestInit {
+    @Test
+
+    public void checkLanguageBtn(){
+        openUrl("https://rozetka.com.ua/ua/");
+        HomePage homePage = new HomePage(driver);
+        LanguageBtnPage languageBtnPage = new  LanguageBtnPage(driver);
+        CheckChangeLanguagePage checkChangeLanguagePage = new CheckChangeLanguagePage(driver);
+        sleep(2);
+        homePage.clickTheBtnOpenMenu().click();
+        sleep(2);
+        languageBtnPage.changeTheLanguage();
+        sleep(2);
+        Assert.assertTrue(checkChangeLanguagePage.ChangeLang().isDisplayed());
+    }
+}
