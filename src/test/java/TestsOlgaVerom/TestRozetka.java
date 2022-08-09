6353803bde968e751b424a1ad4a79d95302c3943
Valentine <valen57tin@gmail.com>
2022-08-09 19:23:24 +0300

    Did the test for Shopify site header, no login, no registration used.

--- src/test/java/TestsOlgaVerom/TestRozetka.java
+++ src/test/java/TestsOlgaVerom/TestRozetka.java
@@ -3,7 +3,7 @@ package TestsOlgaVerom;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import TestsOlgaVerom.pageObjects.RozetkaHomePageHelper;
-import tests.TestInit;
+import BaseClasses.TestInit;
 
 public class TestRozetka extends TestInit {
     @Test
