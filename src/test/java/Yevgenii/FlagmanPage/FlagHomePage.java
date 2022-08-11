a43a2cec3016b1eb5722464759e77524fe7c9536
Jack0ns <mailuabox@ukr.net>
2022-08-11 22:02:43 +0300

    sorting and 3 tests

---
+++ src/test/java/Yevgenii/FlagmanPage/FlagHomePage.java
@@ -0,0 +1,50 @@
+package Yevgenii.FlagmanPage;
+
+import Yevgenii.FlagmanElements.FlagHomeElements;
+import org.openqa.selenium.WebDriver;
+
+public class FlagHomePage extends FlagHomeElements {
+
+    public FlagHomePage(WebDriver driver){
+        super(driver);
+    }
+
+    public void open(){
+        driver.get("https://www.flagman.kiev.ua/");
+    }
+
+    public FlagHomePage selectFieldInput(){
+        getFieldInput().sendKeys("атрактант");
+        return this;
+    }
+
+    public FlagHomePage selectFieldInput1(){
+        getFieldInput().sendKeys("@#$%^");
+        return this;
+    }
+
+    public FlagHomePage selectSearchBtn(){
+        clickSearchBtn().click();
+        return this;
+    }
+
+   public FlagHomePage selectLanguageBtnRu(){
+   clickLanguageBtnRu().click();
+   return this;
+   }
+
+    public FlagHomePage selectLanguageBtnUa(){
+        clickLanguageBtnUa().click();
+        return this;
+    }
+
+    public FlagHomePage selectCatalogBtn(){
+        getCatalogBtn().click();
+        return this;
+    }
+
+    public FlagHomePage selectCatalogEholotBtn(){
+        getCatalogEholotBtn().click();
+        return this;
+    }
+}
