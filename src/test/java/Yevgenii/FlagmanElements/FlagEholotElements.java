c2d0c35da5125177410c8cd9c1ac30c0c9a8b8b8
Jack0ns <mailuabox@ukr.net>
2022-08-12 19:21:37 +0300

    TestCallBack

--- src/test/java/Yevgenii/FlagmanElements/FlagEholotElements.java
+++ src/test/java/Yevgenii/FlagmanElements/FlagEholotElements.java
@@ -7,23 +7,23 @@ import org.openqa.selenium.WebElement;
 
 public class FlagEholotElements extends BasePage {
 
-    public FlagEholotElements(WebDriver driver){
+    public FlagEholotElements(WebDriver driver) {
         super(driver);
     }
 
-    public WebElement clickEholotBtn(){
+    public WebElement clickEholotBtn() {
         return $("//a[text()='Ехолоти']");
     }
 
-    public WebElement clickSortingEholotBtn(){
+    public WebElement clickSortingEholotBtn() {
         return $("//div[contains(@class,'hidden-xs')]//child::select");
     }
 
-    public WebElement clickCompareBtn(){
+    public WebElement clickCompareBtn() {
         return $("//div[contains(@goods-data-gtm, 'Lucky FF 518')]//child::ul/li[@name='compare_details']");
     }
 
-    public WebElement getResaultCompareBtn(){
+    public WebElement getResaultCompareBtn() {
         return $("//span[@class='compare-count']");
     }
 }
--- src/test/java/Yevgenii/FlagmanElements/FlagHomeElements.java
+++ src/test/java/Yevgenii/FlagmanElements/FlagHomeElements.java
@@ -1,53 +1,76 @@
 package Yevgenii.FlagmanElements;
 
 import BaseClasses.BasePage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class FlagHomeElements extends BasePage {
 
-    public FlagHomeElements(WebDriver driver){
+    public FlagHomeElements(WebDriver driver) {
         super(driver);
     }
 
-    public WebElement clickLanguageBtnRu(){
-        return driver.findElement(By.xpath("//a[contains(text(),'Рус')]"));
+    public WebElement clickLanguageBtnRu() {
+        return $("//a[contains(text(),'Рус')]");
     }
 
-    public WebElement clickLanguageBtnUa(){
-        return driver.findElement(By.xpath("//a[contains(text(),'Укр')]"));
+    public WebElement clickLanguageBtnUa() {
+        return $("//a[contains(text(),'Укр')]");
     }
 
-    public WebElement getResaultLenguagRu(){
-        return driver.findElement(By.xpath("//a[contains(text(),'Войти с паролем')][1]"));
+    public WebElement getResaultLenguagRu() {
+        return $("//a[contains(text(),'Войти с паролем')][1]");
     }
 
-    public WebElement getResaultLenguagUa(){
-        return driver.findElement(By.xpath("//a[contains(text(),'Увійти з паролем')][1]"));
+    public WebElement getResaultLenguagUa() {
+        return $("//a[contains(text(),'Увійти з паролем')][1]");
     }
 
-    public WebElement getFieldInput(){
-        return driver.findElement(By.xpath("//input[@id='search-text']"));
+    public WebElement getFieldInput() {
+        return $("//input[@id='search-text']");
     }
 
-    public WebElement clickSearchBtn(){
-        return driver.findElement(By.xpath("//button[@class='search'][1]"));
+    public WebElement clickSearchBtn() {
+        return $("//button[@class='search'][1]");
     }
 
-    public WebElement getVisibilityText(){
-        return driver.findElement(By.xpath("//span[text()='«атрактант»']"));
+    public WebElement getVisibilityText() {
+        return $("//span[text()='«атрактант»']");
     }
 
-    public WebElement getVisibilityNotText(){
-        return driver.findElement(By.xpath("//p[text()='Нічого не знайдено']"));
+    public WebElement getVisibilityNotText() {
+        return $("//p[text()='Нічого не знайдено']");
     }
 
-    public WebElement getCatalogBtn(){
-        return driver.findElement(By.xpath("//i[@class='icon icon-menu_item_1']"));
+    public WebElement getCatalogBtn() {
+        return $("//i[@class='icon icon-menu_item_1']");
     }
 
-    public WebElement getCatalogEholotBtn(){
-        return driver.findElement(By.xpath("//div[contains(text(),'Ехолоти, навігатори і аксесуари')]"));
+    public WebElement getCatalogEholotBtn() {
+        return $("//div[contains(text(),'Ехолоти, навігатори і аксесуари')]");
+    }
+
+    public WebElement getCallBackHeader() {
+        return $("//a[@id='callback-header']");
+    }
+
+    public WebElement getInputNamber() {
+        return $("//input[@name='contact']");
+    }
+
+    public WebElement getInputName() {
+        return $("//input[@name='name']");
+    }
+
+    public WebElement getinputComment() {
+        return $("//textarea[@name='message']");
+    }
+
+    public WebElement getCallBtn() {
+        return $("//button[@class='btn']");
+    }
+
+    public WebElement getVisibilityApplicationCall() {
+        return $("//div[@name='msg']");
     }
 }
