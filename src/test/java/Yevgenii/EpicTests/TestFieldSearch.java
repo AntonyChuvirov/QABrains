745ea15fc65e3786dfdfb097d61e213e18ad542d
Jack0ns <mailuabox@ukr.net>
2022-08-16 19:00:56 +0300

    Apruvnite my test please

---
+++ src/test/java/Yevgenii/EpicTests/TestFieldSearch.java
@@ -0,0 +1,22 @@
+package Yevgenii.EpicTests;
+
+import BaseClasses.TestInit;
+import Yevgenii.EpikElements.HomeElements;
+import Yevgenii.EpikPage.HomePage;
+import jdk.jfr.Description;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestFieldSearch extends TestInit {
+
+    @Test
+    @Description("Checking the term of product search")
+    public void checkFieldSearch() {
+        HomePage homePage = new HomePage(driver);
+        HomeElements homeElements = new HomeElements(driver);
+
+        homePage.open();
+        homePage.inputSearchField();
+        Assert.assertTrue(homeElements.getVisibilityProduct().size() > 10);
+    }
+}
