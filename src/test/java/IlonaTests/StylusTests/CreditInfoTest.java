3aa58b7a3c7193fedf4c2b555bf2262a838aa256
Ilona27 <ilona.kohan2003@gmail.com>
2022-08-16 14:39:32 +0300

    CreditBtn

---
+++ src/test/java/IlonaTests/StylusTests/CreditInfoTest.java
@@ -0,0 +1,11 @@
+package IlonaTests.StylusTests;
+
+import BaseClasses.TestInit;
+
+public class CreditInfoTest extends TestInit {
+
+    public void CreditInfoCheck(){
+        openUrl("https://stylus.ua/uk/");
+
+    }
+}
