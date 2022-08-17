6915e2eed01921527f572a713b42637b69c26213
TsimokhDmytro <tsimokh_@ukr.net>
2022-08-17 18:23:57 +0300

    first commit

--- src/test/java/TestDima/Pages/HomePage.java
+++ src/test/java/TestDima/Pages/HomePage.java
@@ -44,4 +44,8 @@ public class HomePage extends HomePageElements {
     public void clickBtnInCatalogTourismAndFishing(){
         getBtnInCatalogTourismAndFishing().click();
     }
+
+    public void clickBtnElectronicCigarettes(){
+        getBtnElectronicCigarettes().click();
+    }
 }
